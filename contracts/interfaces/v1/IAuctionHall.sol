ecc51e81a7d77aa60faea221a487ab51bcdf37b9
Julien Marchand <julien.marchand4@gmail.com>
2022-08-08 14:39:14 +0000

    Change event order

--- contracts/interfaces/v1/IAuctionHall.sol
+++ contracts/interfaces/v1/IAuctionHall.sol
@@ -21,8 +21,8 @@ interface IAuctionHall {
     // Events
     event AuctionInitiated(address indexed positionAddr);
     event BidTanto(address indexed positionAddr, address indexed ruler, uint256 bidSize, uint256 priorityFee);
+    event Phase1Rollback(address indexed positionAddr);
     event BidKatana(address indexed positionAddr, address indexed ruler, uint256 debtSize, uint256 usedCash, uint256 dexCoverReward);
     event AuctionFinished(address indexed positionAddr, uint256 indexed phase);
-    event Phase1Rollback(address indexed positionAddr);
     event Retrieve(address indexed positionAddr, uint256 stableTokenSize, uint256 debtTokenSize, uint256 priorityFee);
 }

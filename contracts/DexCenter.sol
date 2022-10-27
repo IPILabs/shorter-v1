d5ac70944157ccb2804f99e7e40a1bc3a256198e
Jungho Bang <jungho.bang@coinbase.com>
2022-10-27 03:41:38 -0700

    add TWAP support to PriceOracle

--- contracts/DexCenter.sol
+++ contracts/DexCenter.sol
@@ -5,6 +5,7 @@ pragma experimental ABIEncoderV2;
 import "./interfaces/ISRC20.sol";
 import {SafeERC20 as SafeToken} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./libraries/Path.sol";
+import "./libraries/TickMath.sol";
 import "./interfaces/IUSDT.sol";
 import "./interfaces/IDexCenter.sol";
 import "./interfaces/uniswapv2/IUniswapV2Factory.sol";
@@ -21,13 +22,16 @@ contract DexCenter is Affinity, IDexCenter {
     using SafeToken for ISRC20;
     using Path for bytes;
 
+    uint256 public twapInterval = 60;
     mapping(address => bool) public override entitledSwapRouters;
     mapping(address => bool) public override isSwapRouterV3;
+    mapping(address => bool) public isMiddleToken;
 
     constructor(address _SAVIOR) public Affinity(_SAVIOR) {}
 
     function sellShort(SellShortParams memory params) external returns (uint256 usdAmount) {
         address[] memory _path = params.path.getRouter();
+        _checkMiddleTokens(_path);
         ISRC20 tokenIn = ISRC20(_path[0]);
         ISRC20 tokenOut = ISRC20(_path[_path.length - 1]);
 
@@ -54,6 +58,7 @@ contract DexCenter is Affinity, IDexCenter {
 
     function buyCover(BuyCoverParams memory params) external returns (uint256 amountIn) {
         address[] memory _path = params.path.getRouter();
+        _checkMiddleTokens(_path);
         (ISRC20 tokenIn, ISRC20 tokenOut) = params.isSwapRouterV3 ? (ISRC20(_path[_path.length - 1]), ISRC20(_path[0])) : (ISRC20(_path[0]), ISRC20(_path[_path.length - 1]));
         uint256 tokenInBal = tokenIn.balanceOf(address(this));
         uint256 tokenOutBal = tokenOut.balanceOf(params.to);
@@ -80,38 +85,6 @@ contract DexCenter is Affinity, IDexCenter {
         }
     }
 
-    function _swapExactTokensForTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address swapRouter,
-        address to,
-        address[] memory path
-    ) internal returns (uint256 amountOut) {
-        uint256[] memory amounts = IUniswapV2Router02(swapRouter).swapExactTokensForTokens(amountIn, amountOutMin, path, to, block.timestamp);
-        amountOut = amounts[amounts.length - 1];
-    }
-
-    function _swapTokensForExactTokens(
-        uint256 amountOut,
-        uint256 amountInMax,
-        address swapRouter,
-        address to,
-        address[] memory path
-    ) internal returns (uint256 amountIn) {
-        uint256[] memory amounts = IUniswapV2Router02(swapRouter).swapTokensForExactTokens(amountOut, amountInMax, path, to, block.timestamp);
-        amountIn = amounts[0];
-    }
-
-    function _exactInput(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address swapRouter,
-        address to,
-        bytes memory path
-    ) internal returns (uint256 amountOut) {
-        amountOut = IV3SwapRouter(swapRouter).exactInput(IV3SwapRouter.ExactInputParams({path: path, recipient: to, amountIn: amountIn, amountOutMinimum: amountOutMin}));
-    }
-
     function _exactOutput(
         uint256 amountOut,
         uint256 amountInMax,
@@ -122,31 +95,17 @@ contract DexCenter is Affinity, IDexCenter {
         amountIn = IV3SwapRouter(swapRouter).exactOutput(IV3SwapRouter.ExactOutputParams({path: path, recipient: to, amountOut: amountOut, amountInMaximum: amountInMax}));
     }
 
-    function getV2Price(address swapRouter, address[] memory path) external view override returns (uint256 price) {
-        IUniswapV2Factory swapFactory = IUniswapV2Factory(IUniswapV2Router02(swapRouter).factory());
-
-        price = 1e18;
-        for (uint256 i = 0; i < path.length - 1; i++) {
-            address pairAddr = swapFactory.getPair(path[i], path[i + 1]);
-            (uint112 reserve0, uint112 reserve1, ) = IUniswapV2Pair(pairAddr).getReserves();
-            address tokenIn = IUniswapV2Pair(pairAddr).token0();
-            uint256 tokenInDecimals = uint256(ISRC20(path[i]).decimals());
-            uint256 tokenOutDecimals = uint256(ISRC20(path[i + 1]).decimals());
-            uint256 _price = tokenIn == path[i] ? uint256(reserve1).mul(10**(tokenInDecimals.add(18).sub(tokenOutDecimals))).div(uint256(reserve0)) : uint256(reserve0).mul(10**(tokenInDecimals.add(18).sub(tokenOutDecimals))).div(uint256(reserve1));
-            price = price.mul(_price).div(1e18);
-        }
-    }
-
-    function getV3Price(
+    function getTokenPrice(
         address swapRouter,
         address[] memory path,
         uint24[] memory fees
     ) external view override returns (uint256 price) {
+        require(isSwapRouterV3[swapRouter], "Dex: Invalid swapRouter");
         IUniswapV3Factory swapFactory = IUniswapV3Factory(IV3SwapRouter(swapRouter).factory());
         price = 1e18;
         for (uint256 i = 0; i < fees.length; i++) {
             address poolAddr = swapFactory.getPool(path[i], path[i + 1], fees[i]);
-            (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(poolAddr).slot0();
+            uint160 sqrtPriceX96 = _getSqrtTWAP(poolAddr);
             address token0 = IUniswapV3Pool(poolAddr).token0();
             uint256 token0Decimals = uint256(ISRC20(token0).decimals());
             uint256 token1Decimals = path[i] == token0 ? uint256(ISRC20(path[i + 1]).decimals()) : uint256(ISRC20(path[i]).decimals());
@@ -197,6 +156,85 @@ contract DexCenter is Affinity, IDexCenter {
     function updateSwapRouterV3(address[] memory _swapRouters, bool _isSwapRouterV3) external isKeeper {
         for (uint256 i = 0; i < _swapRouters.length; i++) {
             isSwapRouterV3[_swapRouters[i]] = _isSwapRouterV3;
+        }
+    }
+
+    function addMiddleTokens(address[] calldata newMiddleTokens) external isKeeper {
+        uint256 middleTokenSize = newMiddleTokens.length;
+        for (uint256 i = 0; i < middleTokenSize; i++) {
+            isMiddleToken[newMiddleTokens[i]] = true;
+        }
+    }
+
+    function removeMiddleTokens(address[] calldata _middleTokens) external isKeeper {
+        uint256 middleTokenSize = _middleTokens.length;
+        for (uint256 i = 0; i < middleTokenSize; i++) {
+            isMiddleToken[_middleTokens[i]] = false;
+        }
+    }
+
+    function updateTwapInterval(uint256 newTwapInterval) external isSavior {
+        twapInterval = newTwapInterval;
+    }
+
+    function _swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address swapRouter,
+        address to,
+        address[] memory path
+    ) internal returns (uint256 amountOut) {
+        uint256[] memory amounts = IUniswapV2Router02(swapRouter).swapExactTokensForTokens(amountIn, amountOutMin, path, to, block.timestamp);
+        amountOut = amounts[amounts.length - 1];
+    }
+
+    function _swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address swapRouter,
+        address to,
+        address[] memory path
+    ) internal returns (uint256 amountIn) {
+        uint256[] memory amounts = IUniswapV2Router02(swapRouter).swapTokensForExactTokens(amountOut, amountInMax, path, to, block.timestamp);
+        amountIn = amounts[0];
+    }
+
+    function _exactInput(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address swapRouter,
+        address to,
+        bytes memory path
+    ) internal returns (uint256 amountOut) {
+        amountOut = IV3SwapRouter(swapRouter).exactInput(IV3SwapRouter.ExactInputParams({path: path, recipient: to, amountIn: amountIn, amountOutMinimum: amountOutMin}));
+    }
+
+    function _checkMiddleTokens(address[] memory _path) internal view {
+        if (_path.length > 2) {
+            for (uint256 i = 1; i < _path.length - 1; i++) {
+                require(isMiddleToken[_path[i]], "DexCenter: Invaild path");
+            }
+        }
+    }
+
+    function _getSqrtTWAP(address uniswapV3Pool) internal view returns (uint160 sqrtPriceX96) {
+        uint32 _twapInterval = uint32(twapInterval);
+        IUniswapV3Pool pool = IUniswapV3Pool(uniswapV3Pool);
+        (, , uint16 index, uint16 cardinality, , , ) = pool.slot0();
+        (uint32 targetElementTime, , , bool initialized) = pool.observations((index + 1) % cardinality);
+        if (!initialized) {
+            (targetElementTime, , , ) = pool.observations(0);
+        }
+        uint32 delta = uint32(block.timestamp) - targetElementTime;
+        if (delta == 0) {
+            (sqrtPriceX96, , , , , , ) = pool.slot0();
+        } else {
+            if (delta < _twapInterval) _twapInterval = delta;
+            uint32[] memory secondsAgos = new uint32[](2);
+            secondsAgos[0] = _twapInterval;
+            secondsAgos[1] = 0;
+            (int56[] memory tickCumulatives, ) = pool.observe(secondsAgos);
+            sqrtPriceX96 = TickMath.getSqrtRatioAtTick(int24((tickCumulatives[1] - tickCumulatives[0]) / int56(uint56(_twapInterval))));
         }
     }
 }
